3f46fdce25168b16889f951b8e487abc318ec6a2
serhii <serezhalytvynenko@gmail.com>
2023-05-29 11:34:01 +0200

    correct validation

--- src/groups-name-validation.js
+++ src/groups-name-validation.js
@@ -15,33 +15,18 @@ const arrGroups = [
 ];
 
 function nameValidation(arr) {
-  const reg = /([A-Z]{2}|[a-z]{2})(20)(\d{2})(.*)/;
+  const reg = /([a-z]{2}|[A-Z]{2})20\d{2}.*/;
+  const results = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (reg.exec(arr[i]) !== null) {
-      console.log(`${arr[i]} success`);
+  for (const item of arr) {
+    if (reg.test(item)) {
+      results.push(`${item} success`);
     } else {
-      console.log(`${arr[i]} error`);
+      results.push(`${item} error`);
     }
   }
+
+  return results;
 }
-nameValidation(arrGroups);
 
-//Второй вриант без clg внутри функции
-
-// function nameValidation(arr) {
-//   const reg = /([A-Z]{2}|[a-z]{2})(20)(\d{2})(.*)/;
-//   const results = [];
-
-//   for (let i = 0; i < arr.length; i++) {
-//     if (reg.exec(arr[i]) !== null) {
-//       results.push(`${arr[i]} success`);
-//     } else {
-//       results.push(`${arr[i]} error`);
-//     }
-//   }
-
-//   return results;
-// }
-
-// console.log(nameValidation(arrGroups));
+console.log(nameValidation(arrGroups));
